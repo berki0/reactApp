f5f45f57442a0571fc9a32de1a9fb1cbfb533931
berki0 <107307903+berki0@users.noreply.github.com>
2024-04-22 22:44:46 +0300

    add

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
+import { Fragment } from "react/jsx-runtime";
 
-function ListGroup(){
-    return <ul class="list-group">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Morbi leo risus</li>
-    <li class="list-group-item">Porta ac consectetur ac</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>;
+function ListGroup() {
+
+    let cities = [
+        "New York",
+        "Los Angeles",
+        "Chicago",
+        "Houston",
+        "Phoenix",
+        "Philadelphia",
+        "San Antonio",
+        "San Diego",
+        "Dallas",
+        "San Jose",
+        "Austin",
+        "Jacksonville",
+        "San Francisco",
+        "Indianapolis",
+        "Columbus",
+        "Fort Worth",
+        "Charlotte",
+        "Seattle",
+        "Denver",
+        "Washington",
+        "Boston",
+        "El Paso",
+        "Nashville",
+        "Detroit",
+        "Oklahoma City",
+        "Portland",
+        "Las Vegas",
+        "Memphis",
+        "Louisville",
+        "Baltimore",
+        "Milwaukee",
+        "Albuquerque",
+        "Tucson",
+        "Fresno",
+        "Sacramento",
+        "Kansas City",
+        "Long Beach",
+        "Mesa",
+        "Atlanta",
+        "Colorado Springs",
+        "Virginia Beach",
+        "Raleigh",
+        "Omaha",
+        "Miami",
+        "Oakland",
+        "Minneapolis",
+        "Tulsa",
+        "Wichita",
+        "New Orleans",
+    ];
+    //  cities = [];
+
+    const getMessege = () => {
+        return cities.length === 0 ? <p> No cities found</p> : null
+    }
+
+    return (
+        <Fragment>
+            <h1> List Group</h1>
+            {getMessege}
+            <ul className="list-group">
+                {cities.map((city) => (
+                    <li className="list-group-item" key={city} onClick={}>   {city}</li>
+                ))}
+            </ul>
+        </Fragment>
+    )
 }
 export default ListGroup;
